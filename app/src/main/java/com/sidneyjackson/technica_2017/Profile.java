53ba379a429d4830408eacdf036dc2c863de71a4
Sidney Jackson <werty6400@gmail.com>
2017-11-04 17:15:06 -0400

    Added Profile Class.

---
+++ app/src/main/java/com/sidneyjackson/technica_2017/Profile.java
@@ -0,0 +1,40 @@
+package com.sidneyjackson.technica_2017;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sidneyjackson on 11/4/17.
+ */
+
+public class Profile {
+
+    String first_Name = "";
+    String middle_Name = "";
+    String last_Name = "";
+
+    String linkedInLink = "";
+    String emailAddress = "";
+
+    List<Education> educationHistory = new ArrayList<>();
+    String areaOfInterest = "";
+
+    boolean[] peerInterest = {false, false, false};
+
+    String businessName = "";
+    String businessArea = "";
+    String missionStatement;
+
+    String region = "";
+    String country = "";
+
+    List<String> areasOfMerit = new ArrayList<>();
+    List<String> areasOfImprovement = new ArrayList<>();
+
+    public class Education {
+        String school = "";
+        String discipline = "";
+        String degree = "";
+        int year = 0;
+    }
+}
