ecae467670e94895cc735103946b12691290135f
Sidney Jackson <werty6400@gmail.com>
2017-11-05 11:01:48 -0500

    Updates Home Page Fragment to inflate static news feed page.

--- app/src/main/java/com/sidneyjackson/technica_2017/HomeFragment.java
+++ app/src/main/java/com/sidneyjackson/technica_2017/HomeFragment.java
@@ -20,9 +20,6 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private ListView webLinkListView;
-    protected static ArrayList<WebLinkObject> webLinkItems;
-    protected static WebLinkAdapter wlAdapter;
     protected View rootView;
 
     public HomeFragment() {
@@ -43,11 +40,6 @@ public class HomeFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        webLinkItems = populateWebLinkList();
-
-        wlAdapter = new WebLinkAdapter(getActivity(), R.layout.web_link_row, webLinkItems);
-        webLinkListView.setAdapter(wlAdapter); // Layout File
-
         getActivity().setTitle("News Feed");
     }
 
@@ -55,34 +47,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        rootView = inflater.inflate(R.layout.fragment_home, container, false);
-
-        webLinkListView = (ListView) rootView.findViewById(R.id.web_link_list_view);
-
-        webLinkListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Do Nothing
-            }
-        });
+        rootView = inflater.inflate(R.layout.news_feed_example, container, false);
 
         return rootView;
-    }
-
-    public ArrayList<WebLinkObject> populateWebLinkList() {
-        ArrayList<WebLinkObject> webLinks = new ArrayList<>();
-
-
-        Uri uri_1 = Uri.parse("android.resource://com.sidneymjackson.technica_2017/R.drawable.web_img_3");
-        Uri uri_2 = Uri.parse("android.resource://com.sidneymjackson.technica_2017/R.drawable.web_img_1");
-        Uri uri_3 = Uri.parse("android.resource://com.sidneymjackson.technica_2017/R.drawable.web_img_2");
-
-        WebLinkObject webLink1 = new WebLinkObject(uri_1, "How to Manage Money", "Managing money can be tough, but with this guide... ", "moneymanager.com");
-        WebLinkObject webLink2 = new WebLinkObject(uri_2, "How to Fundraise", "Fundraising can be tough, but with this guide... ", "fundmydream.com");
-        WebLinkObject webLink3 = new WebLinkObject(uri_3, "How to Reach Out", "Reaching out can be tough, but with this guide... ", "allinthistogether.com");
-
-        return webLinks;
     }
 
 }
