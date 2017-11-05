37247f4a830b7619dde5220b6705593bbd099764
Sidney Jackson <werty6400@gmail.com>
2017-11-05 10:26:20 -0500

    Added WebLink Object

--- app/src/main/java/com/sidneyjackson/technica_2017/HomeFragment.java
+++ app/src/main/java/com/sidneyjackson/technica_2017/HomeFragment.java
@@ -7,106 +7,82 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    private View rootView;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private ListView webLinkListView;
+    protected static ArrayList<WebLinkObject> webLinkItems;
+    protected static WebLinkAdapter wlAdapter;
+    protected View rootView;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        getActivity().setTitle(R.string.home_title);
-        return rootView;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void onStart() {
+        super.onStart();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        getActivity().setTitle(R.string.home_title);
+
+        webLinkItems = populateWebLinkList();
+
+        wlAdapter = new WebLinkAdapter(getActivity(), R.layout.web_link_row, webLinkItems);
+        webLinkListView.setAdapter(wlAdapter); // Layout File
+
+        getActivity().setTitle("News Feed");
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        webLinkListView = (ListView) rootView.findViewById(R.id.web_link_list_view);
+
+        webLinkListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Do Nothing
+            }
+        });
+
+        return rootView;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public ArrayList<WebLinkObject> populateWebLinkList() {
+        ArrayList<WebLinkObject> webLinks = new ArrayList<>();
+
+
+        Uri uri_1 = Uri.parse("android.resource://com.sidneymjackson.technica_2017/R.drawable.web_img_3");
+        Uri uri_2 = Uri.parse("android.resource://com.sidneymjackson.technica_2017/R.drawable.web_img_1");
+        Uri uri_3 = Uri.parse("android.resource://com.sidneymjackson.technica_2017/R.drawable.web_img_2");
+
+        WebLinkObject webLink1 = new WebLinkObject(uri_1, "How to Manage Money", "Managing money can be tough, but with this guide... ", "moneymanager.com");
+        WebLinkObject webLink2 = new WebLinkObject(uri_2, "How to Fundraise", "Fundraising can be tough, but with this guide... ", "fundmydream.com");
+        WebLinkObject webLink3 = new WebLinkObject(uri_3, "How to Reach Out", "Reaching out can be tough, but with this guide... ", "allinthistogether.com");
+
+        return webLinks;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
